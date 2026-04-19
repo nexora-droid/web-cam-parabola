6522b18bf22c7ab5773afc5bd1a346a4fc794fe9
Ashkesh-D <ashlesh.deshmukh.sg@gmail.com>
2026-04-19 12:56:18 +0800

    Added license info

--- main.py
+++ main.py
@@ -18,3 +18,13 @@ with st.sidebar:
         st.session_state = "FAQ"
     if st.button(label="Options", icon="⚙️", use_container_width=True):
         st.session_state = "Options"
+    st.divider(width="stretch")
+    st.markdown("This project is maintained by [@nexora-droid](https://www.github.com/nexora-droid), under a [personal license](https://github.com/nexora-droid/web-cam-parabola/blob/main/LICENSE.md) proihibitng the usage of this project for commerical uses.")
+    st.write("All code is publicly available at [the repository](https://github.com/nexora-droid/web-cam-parabola/).")
+if st.session_state == "Home":
+    st.title("Home")
+    st.text("Web-bola is a free, open-source project that runs on Python to track the motion of an object using your webcam and turns it into a parabola graph.")
+if st.session_state == "FAQ":
+    st.title("FAQ")
+if st.session_state == "Options":
+    st.title("Options")
