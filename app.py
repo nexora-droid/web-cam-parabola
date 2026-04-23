bae395c2e9d4f2aeb04ad5e76b6b4f1a98fd5b72
nexora-droid <ashlesh.deshmukh.sg@gmail.com>
2026-04-23 19:14:46 +0800

    OpenCV based system started

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
 from git import Repo
 import arrow
 import os
+import av
+import cv2
 
+class Processor(VideoProcessorBase):
+    def __init__(self):
+        self.points =[]
+    def recv(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        lower = (35, 50, 50)
+        upper = (85, 255, 255 )
+        mask = cv2.inRange(hsv, lower, upper)
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        biggest = max(contours, key=cv2.contourArea)
 @st.fragment(run_every="1s")
 def show_sidebar_footer():
     repo = Repo(os.getcwd(), search_parent_directories=True)
@@ -19,7 +32,7 @@ def show_sidebar_footer():
     st.write("All code is publicly available at [the repository](https://github.com/nexora-droid/web-cam-parabola/).")
     st.divider(width="stretch")
     timestamp = commit_date
-    past_time = arrow.get(timestamp)
+    past_time = arrow.aget(timestamp)
     relative_time = past_time.humanize()
     st.markdown(f"**Last commit:** [`{commit_hash[:7]}`]({commit_link}), '_{commit_message.strip()}_'   {relative_time} by {commit_author}")
 
@@ -38,9 +51,10 @@ if st.session_state["nav_page"] == "Home":
     st.text("Web-bola is a free, open-source project that runs on Python to track the motion of an object using your webcam and turns it into a parabola graph.")
     st.divider(width="stretch")
     st.subheader("View your camera")
-    webrtc_streamer(key="video", media_stream_constraints={"video":True, "audio":False})
+    webrtc_streamer(key="video", media_stream_constraints={"video":True, "audio":False}, video_processor_factory=Processor)
     st.text("Press start to start your camera. If you are using an external camera, remember to select it in the select device option")
 if st.session_state["nav_page"] == "FAQ":
     st.title("FAQ")
 if st.session_state["nav_page"] == "Options":
     st.title("Options")
+
